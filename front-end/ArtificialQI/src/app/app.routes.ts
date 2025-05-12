010388f6949083052d712bc219dfbbf14f10f33f
guirong <guirong.lan@studenti.unipd.it>
2025-05-12 22:09:25 +0200

    comments to the routes

--- front-end/ArtificialQI/src/app/app.routes.ts
+++ front-end/ArtificialQI/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
-
+//import { datasetContentPageComponent } from './dataset-content-page/dataset-content-page.component';
+// path: 'datasetContentPage', component: datasetContentPageComponent
 export const routes: Routes = [];

80e2964f7fa13672774fceab26e155fee997686a
Marko Peric <peric182019@gmail.com>
2025-05-20 17:27:12 +0200

    altre aggiunte

--- front-end/ArtificialQI/src/app/app.config.ts
+++ front-end/ArtificialQI/src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HomePageComponent } from './features/home/home-page/home-page.component';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
+
---
+++ front-end/ArtificialQI/src/app/core/services/test.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { TestDto } from '../models/test-dto.model';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class TestService {
+    constructor(private http: HttpClient) {}
+
+    getTest(ID: number): Observable<TestDto> {
+        return this.http.get<TestDto>(`/test/${ID}`);
+    }
+
+    saveTest(test: TestDto): Observable<TestDto> {
+        return this.http.post<TestDto>('/test', test);
+    }
+
+    compareTest(test1: TestDto, test2: TestDto): Observable<any> {
+        return this.http.post<any>('/test/compare', { test1, test2 });
+    }
+}

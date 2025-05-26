7569e37f1b827b0f8ec27b92c6b6316f99e1bdf7
Francesco Savio <francesco.savio.2@studenti.unipd.it>
2025-05-26 06:29:02 +0200

    aggiunto dependency injection, rotta di prova, sqlalchemy, aggiornato mapper

--- back-end/ArtificialQI/src/app.py
+++ back-end/ArtificialQI/src/app.py
@@ -1,17 +1,28 @@
 import os
-
+from routes.containers import Container
 from flask import Flask, jsonify
 from flask_cors import CORS
 from sqlalchemy import create_engine, text
+from flask_sqlalchemy import SQLAlchemy
 
 DB_URL = os.environ["DB_URL"]
 
 app = Flask(__name__)
 CORS(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///mydatabase.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+container = Container()
+container.wire(modules=["routes.dataset_route"]) 
+setattr(app, "container", container)
+
 
 @app.route("/prova")
 def hello_world():
+
+
     engine = create_engine(DB_URL)
     with engine.connect() as conn:
         results = [

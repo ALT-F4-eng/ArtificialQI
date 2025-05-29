4819a2af09611e5a8c942173e357040bc86fce87
Peric Marko <peric182019@gmail.com>
2025-05-29 16:27:32 +0200

    SQLAlchemy test dataset

--- back-end/ArtificialQI/src/app.py
+++ back-end/ArtificialQI/src/app.py
@@ -1,29 +1,45 @@
+# app.py
+
 import os
 from routes.containers import Container
 from flask import Flask, jsonify
 from sqlalchemy import create_engine, text
-from flask_sqlalchemy import SQLAlchemy
+from src.db_config import db  # usa db separato
+from src.models.dataset_model import DatasetModel  # importa solo il modello
 
 app = Flask(__name__)
 
-# Usa la variabile d'ambiente DB_URL
+# Config DB
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)  # inizializza db con l'app
 
+
+# DI Container
 container = Container()
-container.wire(modules=["routes.dataset_route"]) 
+container.wire(modules=["routes.dataset_route"])
 setattr(app, "container", container)
-
-DB_URL = os.environ.get("DB_URL")
 
 @app.route("/prova")
 def hello_world():
     db_url = os.getenv("DB_URL")
-    if db_url is None:
-        raise ValueError("DATABASE_URL is not set")
     engine = create_engine(db_url)
     with engine.connect() as conn:
-        results = [tuple(row) for row in conn.execute(text("SELECT * FROM PROVA")).fetchall()]
+        results = [tuple(row) for row in conn.execute(text("SELECT * FROM Dataset")).fetchall()]
     return jsonify(results)
+
+@app.route("/test-db")
+def test_db():
+    with app.app_context():  # richiesto da SQLAlchemy
+        new_dataset = DatasetModel(nome="Dataset di test", is_tmp=True)
+        db.session.add(new_dataset)
+        db.session.commit()
+        last = DatasetModel.query.order_by(DatasetModel.id.desc()).first()
+        return {
+            "id": last.id,
+            "nome": last.nome,
+            "is_tmp": last.is_tmp,
+            "data_creazione": str(last.data_creazione),
+            "data_ultima_modifica": str(last.data_ultima_modifica)
+        }

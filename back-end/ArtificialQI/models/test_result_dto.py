c4a0950194c9269768891e7e5c015bbcbdac40ef
Francesco Savio <francesco.savio.2@studenti.unipd.it>
2025-05-31 17:26:42 +0200

    mapper llm e httpconfig

--- back-end/ArtificialQI/models/test_result_dto.py
+++ back-end/ArtificialQI/models/test_result_dto.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from artificialqi.models.qa_dto import QuestionAnswerPairDTO
 
-class TestResultDto(BaseModel):
+class TestResultDTO(BaseModel):
     qa: QuestionAnswerPairDTO
     is_correct: bool
     similarity_score: float

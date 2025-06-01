d40026f3cfbf66263c273fc7845eaa397acb559c
Francesco Savio <francesco.savio.2@studenti.unipd.it>
2025-06-01 17:19:24 +0200

    llm e config mapper sistemati

--- back-end/ArtificialQI/mapper/config_mapper.py
+++ back-end/ArtificialQI/mapper/config_mapper.py
@@ -3,23 +3,26 @@ from artificialqi.core.http_config import HttpConfig, http_config_function_facto
 from artificialqi.models.configuration_dto import ConfigurationDto
 
 
+class ConfigMapper:
 
-def to_domain(dto: ConfigurationDto, id: UUID) -> HttpConfig:
-    return http_config_function_factory(
-        id = id or uuid4(),
-        url = dto.url,
-        key_req = dto.key_req,
-        key_resp = dto.key_resp,
-        header = dto.header,
-        body = dto.body
-    )
+    @staticmethod
+    def to_domain(dto: ConfigurationDto, id: UUID) -> HttpConfig:
+        return http_config_function_factory(
+            id = id or uuid4(),
+            url = dto.url,
+            key_req = dto.key_req,
+            key_resp = dto.key_resp,
+            header = dto.header,
+            body = dto.body
+        )
 
 
-def to_dto(config: HttpConfig) -> ConfigurationDto:
-    return ConfigurationDto(
-        ulr=str(config.url),
-        key_req=config.key_req,
-        key_resp=config.key_resp,
-        header=config.header.to_dict(),  
-        body=config.body.to_dict()
-    )
+    @staticmethod
+    def to_dto(config: HttpConfig) -> ConfigurationDto:
+        return ConfigurationDto(
+            ulr=str(config.url),
+            key_req=config.key_req,
+            key_resp=config.key_resp,
+            header=config.header.to_dict(),  
+            body=config.body.to_dict()
+        )

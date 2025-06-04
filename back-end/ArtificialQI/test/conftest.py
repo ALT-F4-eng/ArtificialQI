3756554bedf28047be3a6f2109813dc47ccdcd19
Pedro Leoni <pedroleoni2001@gmail.com>
2025-06-04 16:36:38 +0200

    Risolve problemi

--- back-end/ArtificialQI/test/conftest.py
+++ back-end/ArtificialQI/test/conftest.py
@@ -1,3 +1,5 @@
+#type: ignore
+
 from datetime import date
 from uuid import UUID, uuid4
 from typing import Any
@@ -10,26 +12,24 @@ def get_dataset_values()->dict[str, Any]:
     return {
         "NAME" : "prova",
         "ID" : UUID('2a004a75-324a-4b17-8cd7-3e7dd2e958e7'),
-        "CREATION_DATE" : date(year="2025", month="6", day="04")
+        "CREATION_DATE" : date(year=2025, month=6, day=4)
     }
 
 @fixture
-def get_dataset_with_specific_id() -> Dataset:
+def get_dataset_with_specific_id(get_dataset_values) -> Dataset:
     
 
     return Dataset(
-        _name=NAME,
-        _id=ID,
-        _creation_date=CREATION_DATE
+        _name=get_dataset_values["NAME"],
+        _id=get_dataset_values["ID"],
+        _creation_date=get_dataset_values["CREATION_DATE"]
     )
 
 @fixture
-def get_dataset_with_random_id() -> Dataset:
-    NAME: str = "prova"
-    CREATION_DATE: date = date.today()
+def get_dataset_with_random_id(get_dataset_values) -> Dataset:
 
     return Dataset(
-        _name=NAME,
+        _name=get_dataset_values["NAME"],
         _id=uuid4(),
-        _creation_date=CREATION_DATE
+        _creation_date=get_dataset_values["CREATION_DATE"]
     )
--- back-end/ArtificialQI/test/core/dataset_test.py
+++ back-end/ArtificialQI/test/core/dataset_test.py
@@ -1,7 +1,5 @@
-from datetime import date
-from uuid import UUID, uuid4
-from pytest import fixture
 from core.dataset import Dataset
+import pytest
 
 class TestEqMethod:
 
@@ -28,26 +26,20 @@ class TestEqMethod:
         
 class TestNameSetterMethod:
 
-    def test_with_non_dataset_type(self, get_dataset_with_specific_id: Dataset):
+    def test_with_empty_name(self, get_dataset_with_specific_id: Dataset):
 
         invoc_dataset: Dataset = get_dataset_with_specific_id
 
-        non_dataset_obj: object = object()
+        with pytest.raises(ValueError):
+            invoc_dataset.name = ""
 
-        assert(invoc_dataset == non_dataset_obj)
-
-    def test_with_different_id_dataset(self, get_dataset_with_specific_id: Dataset, get_dataset_with_random_id: Dataset):
+    def test_with_only_space_name(self, get_dataset_with_specific_id: Dataset):
 
         invoc_dataset: Dataset = get_dataset_with_specific_id
-        different_dataset: Dataset = get_dataset_with_random_id
 
-        assert(invoc_dataset == different_dataset)
+        with pytest.raises(ValueError):
+            invoc_dataset.name = "   "
 
-    def test_with_equal_id_dataset(self, get_dataset_with_specific_id: Dataset):
-
-        assert(
-            get_dataset_with_specific_id == get_dataset_with_specific_id
-        )
         
 
 
